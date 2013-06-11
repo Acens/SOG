c9e895d91c0a28db7d7e51482def67d91690e82a
Nicolas <nicolascc.uece@gmail.com>
2013-06-11 11:01:16 -0300

    comecei a fazer a parte do scrum

---
+++ application/views/scrum/projetos_alocados.php
@@ -0,0 +1,12 @@
+<?php $this->load->view('includes/header'); ?>
+<?php $this->load->view('includes/top_menu'); ?>
+
+
+<div class="hero-unit-acens">
+  <h1 class="text-center">Projetos</h1>
+  <p class="text-center">Esses são os projetos onde você esta alocado</p>
+  
+</div>
+
+
+<?php $this->load->view('includes/footer'); ?>
